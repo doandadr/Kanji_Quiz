d0abb56fc7973581a4ee67302e80f47b867f147f
Doanda Dresta Rahma <66405353+Doanda37Rahma@users.noreply.github.com>
2021-10-10 21:45:27 +0700

    refactor: remove unused vars

--- src/shuffleArray.js
+++ src/shuffleArray.js
@@ -5,7 +5,7 @@ export const shuffleArray = (arr) => {
   // console.log(arr.length)
 
   // While there remain elements to shuffle...
-  while (currentIndex != 0) {
+  while (currentIndex !== 0) {
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex)
     currentIndex--
