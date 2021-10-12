cecfc83b869f5e0b8895a8592e81a9b75b849a2e
Doanda Dresta Rahma <66405353+Doanda37Rahma@users.noreply.github.com>
2021-10-12 19:28:20 +0700

    feat: added grade and jlpt info

--- src/Kanji.js
+++ src/Kanji.js
@@ -1,13 +1,20 @@
 import React from 'react'
-
+import { ordinal_suffix_of } from './ordinal-suffix-of'
+import data from './kanji-jouyou.json'
 const Kanji = ({ currentVal, totalQs, kanji }) => {
   return (
     <div className='kanji'>
-      <div className='kanji__info'>
+      <div className='kanji__count'>
         <h3>
           <span className='kanji__current'>{currentVal + 1}</span>/
           <span className='kanji__total'>{totalQs}</span>
         </h3>
+      </div>
+      <div className='kanji__grade'>
+        <h3>{`${ordinal_suffix_of(data[kanji].grade)} grade`}</h3>
+      </div>
+      <div className='kanji__jlpt'>
+        <h3>{data[kanji].jlpt_new && `JLPT N${data[kanji].jlpt_new}`}</h3>
       </div>
       <h2 className='kanji__character'>{kanji}</h2>
     </div>
